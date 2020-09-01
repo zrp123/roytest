502983dc167823e3c0eea6a39aa13dd133f39c48
934594331 <934594331@qq.com>
2020-09-01 21:24:32 +0800

    项目初始创建

--- src/main/java/com/roy/roytest/controller/TestController.java
+++ src/main/java/com/roy/roytest/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping(value = "/apo/doit")
     public String doit(){
-        //测试的一个项目
+        //测试的一个项目，测试
         return "doit_ok";
     }
 
